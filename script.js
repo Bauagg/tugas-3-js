5c0893f4883e8caf66be044335dbe18ad80f682d
A. Mambaus Sholihin <bauagg87@gmail.com>
2023-03-07 10:31:44 +0700

    baus

---
+++ script.js
@@ -0,0 +1,44 @@
+// condition fariabel true / false
+const harga = 50.000
+const baju = harga < 100.000 ? "murah" : "mahal";
+console.log(baju)
+
+// condition fariabel true
+const kaos = "M"
+const ukuranKaos1 = kaos === "M" && "sedang"
+console.log(ukuranKaos1)
+
+// condition fariabel false
+const ukuranKaos2 = kaos === "L" || "tidak cocok ukuranya"
+console.log(ukuranKaos2)
+
+// data
+const celana = [
+    {
+        nama: "jeans",
+        harga: 50.000
+    },
+    {
+        nama: "cargo",
+        harga: 100.000
+    },
+    {
+        nama: "chinos",
+        harga: 150.000
+    },
+    {
+        nama: "training",
+        harga: 200.000
+    },
+]
+
+// map (Fungsi method map pada JavaScript adalah untuk membuat array baru dari memanggil sebuah function pada setiap elemen array.)
+const brendCelana = celana.map((brend) => brend.nama)
+console.log(brendCelana)
+
+const listNilai = celana.map((nilai) => nilai.harga + 2.000)
+console.log(listNilai)
+
+// filter (merupakan method array di JavaScript yang berfungsi untuk mencari semua elemen di dalam array yang sesuai dengan kriteria tertentu.)
+const nilai = celana.filter((kategori) => kategori.harga > 50.000)
+console.log(nilai)
